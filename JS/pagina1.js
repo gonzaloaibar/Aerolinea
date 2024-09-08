e930c2781a59ba9b17e690d1df3912fb1071904f
Gon_aibar <aibargonzalo0@gmail.com>
2024-09-07 21:51:09 -0300

    se agrgaron funciones

--- JS/pagina1.js
+++ JS/pagina1.js
@@ -1,15 +1,43 @@
 //EJERCICIO 1
 
 //validacion de la fecha
-document.gentElementById("formulario").addEventListener("sumbit", function(event){
+document.getElementById("formulario").addEventListener("submit", function(event){
     const fecha_actual=new Date();
 
     const fecha_ingresada=new Date(document.getElementById("fecha").value);
 
-    if(fecha_actual > fecha_ingresada){
+    if(fecha_ingresada<fecha_actual ){
         event.preventDefault();
         alert("la fecha del vuelo no puede ser mayor a la fecha actual");
 
     }
 
 })
+
+//validacion del origen y destino del vuelo
+
+function habilitar_opciones(){
+    const destino=document.getElementById("destino");
+    for(let i=0;i<destino.options.length;i++){
+        destino.options[i].disable=false;
+    }
+}
+
+
+
+function verificar_destino(){
+    const origen = document.getElementById("origen").value;
+    let destino = document.getElementById("destino");
+    alert("la opcion es "+origen);
+
+    habilitar_opciones();
+
+    for(let i=0;i<destino.options.length; i++){
+        if(destino.options[i].value===origen){
+            destino.options[i].disable=true;
+        }
+    }
+    
+}
+
+
