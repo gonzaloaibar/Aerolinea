5206d75aa3fbed14b78a4f8c075a9de0cfa6f040
Gon_aibar <aibargonzalo0@gmail.com>
2024-09-09 16:51:59 -0300

    se modifico toda la pagina2

--- JS/pagina2.js
+++ JS/pagina2.js
@@ -1,29 +1,6 @@
-//EJERCICIO 2
 
-/*seleccionar ubicacion segun la clase*/
 
-function habilitar_opciones(){
-    const ubicacion=document.getElementById("ubicacion");
-    for(let i=0;i<ubicacion.options.length;i++){
-        ubicacion.options[i].disabled=false;
-    }
-}
-
-function seleccionar_ubicacion(){
-    const clase=document.getElementById("clase").value;
-    const tipo_clase=document.getElementById("clase");
-    const ubicacion=document.getElementById("ubicacion");
-    //alert("se eligio "+clase);
-
-    habilitar_opciones();
-
-    if(clase===tipo_clase.options[2].value){
-        ubicacion.options[2].disabled=true;
-    }
-
-}
-
-/*limitar hacientos segun la clase*/
+/*limitar hacientos segun la clase
 
 function limitar_numero_hacientos(){
     const clase=document.getElementById("clase").value;
@@ -41,12 +18,36 @@ function limitar_numero_hacientos(){
     }
 
     
+}*/
+
+
+
+
+function habilitar_opciones(i){
+    const ubicacion=document.getElementById(`ubicacion${i}`);
+    for(let i=0;i<ubicacion.options.length;i++){
+        ubicacion.options[i].disabled=false;
+    }
 }
 
-function gestionar_segun_clase(){
-    seleccionar_ubicacion();
+function seleccionar_ubicacion(i){
+    const clase=document.getElementById(`clase${i}`).value;
+    const tipo_clase=document.getElementById(`clase${i}`);
+    const ubicacion=document.getElementById(`ubicacion${i}`);
+    //alert("se eligio "+clase);
 
-    limitar_numero_hacientos();
+    habilitar_opciones(i);
+
+    if(clase===tipo_clase.options[1].value){
+        ubicacion.options[2].disabled=true;
+    }
+
+}
+
+function gestionar_segun_clase(i){
+    seleccionar_ubicacion(i);
+
+    //limitar_numero_hacientos();
 }
 
 
@@ -54,105 +55,94 @@ function gestionar_segun_clase(){
 document.addEventListener('DOMContentLoaded', function() {
     const formulario = document.getElementById('formularioPasajeros');
     const tabla = document.getElementById('tablaPasajeros').querySelector('tbody');
+    const contenedorPasajeros = document.getElementById('contenedorPasajeros');
 
-    /*ampliar campos segun la cantidad de pasajes*/
-    document.getElementById("pasajes").addEventListener("input", function(){
-        const cantidad = parseInt(this.value, 10); // Convertir el valor a número entero
-        const contenedorPasajeros = document.getElementById("contenedor");
+    
+    document.getElementById('cantidadPasajes').addEventListener('input', function() {
+        const cantidad = parseInt(this.value, 10);
         const pasajerosExistentes = contenedorPasajeros.children.length;
 
         if (cantidad > pasajerosExistentes) {
             for (let i = pasajerosExistentes + 1; i <= cantidad; i++) {
-                const pasajeroDiv = document.createElement('div');
-                pasajeroDiv.classList.add('pasajero');
-                pasajeroDiv.setAttribute('id', `pasajero-${i}`);
-                pasajeroDiv.innerHTML = `
-                    <h3>Pasajero ${i}</h3>
-                    <label for="nombre${i}">Nombre y Apellido:</label>
-                    <input type="text" id="nombre${i}" name="nombre${i}" required>
-                    <br>
-                    <label for="dni${i}">DNI:</label>
-                    <input type="text" id="dni${i}" name="dni${i}" required>
-                    <br>
-                    <label for="fecha${i}">Fecha de Nacimiento:</label>
-                    <input type="date" id="fecha${i}" name="fecha${i}" required>
-                    <br><br>
-                    <label for="clase${i}">tipo de clase</label>
-                    <select name="clase${i}" id="clase${i}" required>
-                        <option value="">seleccionar</option>
-                        <option value="economica">ECONOMICA</option>
-                        <option value="ejecutiva">EJECUTIVA</option>
-                    </select>
-                    <br><br>
-                    <label for="ubicacion${i}">ubicacion del haciento</label>
-                    <select name="ubicacion${i}" id="ubicacion${i}">
-                        <option value="">seleccionar</option>
-                        <option value="ventanilla">VENTANILLA</option>
-                        <option value="centro">CENTRO</option>
-                        <option value="pasillo">PASILLO</option>
-                    </select>
-                    <br><br>
-                    <label for="silla${i}">seleccione numero de silla</label>
-                    <input type="number" id="silla${i}" name="silla${i}" min="1" max="50">
-                    <br><br>
-                    <p>seleccione el sexo</p>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                        <label class="form-check-label" for="flexRadioDefault1">
-                        masculino
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                        <label class="form-check-label" for="flexRadioDefault2">
-                        femenino
-                        </label>
-                    </div>
-                `;
-                contenedorPasajeros.appendChild(pasajeroDiv);
+                agregarPasajero(i);
             }
-        }else if (cantidad < pasajerosExistentes) {
+        } else if (cantidad < pasajerosExistentes) {
             for (let i = pasajerosExistentes; i > cantidad; i--) {
-                const pasajeroAEliminar = document.getElementById(`pasajero-${i}`);
-                if (pasajeroAEliminar) {
-                    contenedorPasajeros.removeChild(pasajeroAEliminar);
+                if (i > 1) {
+                    const pasajeroAEliminar = document.getElementById(`pasajero-${i}`);
+                    if (pasajeroAEliminar) {
+                        contenedorPasajeros.removeChild(pasajeroAEliminar);
+                    }
                 }
             }
         }
+    });
 
-    })
+    // Función para agregar un nuevo pasajero
+    function agregarPasajero(numero) {
+        const pasajeroDiv = document.createElement('div');
+        pasajeroDiv.classList.add('pasajero');
+        pasajeroDiv.setAttribute('id', `pasajero-${numero}`);
+        pasajeroDiv.innerHTML = `
+            <h3>Pasajero ${numero}</h3>
+            <label for="nombre${numero}">Nombre y Apellido:</label>
+            <input type="text" id="nombre${numero}" name="nombre${numero}" required><br>
+            <label for="dni${numero}">DNI:</label>
+            <input type="text" id="dni${numero}" name="dni${numero}" required><br>
+            <label for="fecha${numero}">Fecha de Nacimiento:</label>
+            <input type="date" id="fecha${numero}" name="fecha${numero}" required><br>
+            <label for="clase${numero}">Clase:</label>
+            <select id="clase${numero}" name="clase${numero}" onchange="gestionar_segun_clase(${numero})" required>
+                <option value="">seleccionar</option>
+                <option value="Ejecutiva">Ejecutiva</option>
+                <option value="Económica">Económica</option>
+            </select><br>
+            <label for="ubicacion${numero}">ubicacion del haciento</label>
+            <select name="ubicacion${numero}" id="ubicacion${numero}">
+                <option value="">seleccionar</option>
+                <option value="ventanilla">VENTANILLA</option>
+                <option value="centro">CENTRO</option>
+                <option value="pasillo">PASILLO</option>
+             </select>
+            <br><br>
+                
+            <label for="silla${numero}">seleccione numero de silla</label>
+            <input type="number" id="silla${numero}" name="silla${numero}" min="1" max="50">
+            <br><br>
+        `;
+        contenedorPasajeros.appendChild(pasajeroDiv);
+    }
 
+   
     formulario.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevenir el comportamiento por defecto (recargar la página)
+        event.preventDefault(); 
 
-        const cantidad = parseInt(document.getElementById('pasajes').value, 10);
+        const cantidad = parseInt(document.getElementById('cantidadPasajes').value, 10);
         for (let i = 1; i <= cantidad; i++) {
             const nombre = document.getElementById(`nombre${i}`).value;
             const dni = document.getElementById(`dni${i}`).value;
             const fecha = document.getElementById(`fecha${i}`).value;
-            const num_asiento=document.getElementById(`silla${i}`).value;
-            const clase=document.getElementById(`clase${i}`).value;
-            
-            agregarFilaATabla(nombre, dni, fecha, num_asiento, clase);
+            const clase = document.getElementById(`clase${i}`).value;
+            const silla=document.getElementById(`silla${i}`).value;
+
+            agregarFilaATabla(nombre, dni, fecha, clase,silla);
         }
 
-        // Limpiar los campos del formulario después de agregar los datos a la tabla
+        
         formulario.reset();
-        document.getElementById('pasajes').value = 0; // Restablecer el valor de cantidad a 1
+        document.getElementById('cantidadPasajes').value = 1; 
     });
 
-    function agregarFilaATabla(nombre, dni, fecha, num_asiento,clase) {
+    // Función para agregar una fila a la tabla
+    function agregarFilaATabla( nombre, dni, fecha, clase, silla) {
         const fila = document.createElement('tr');
         fila.innerHTML = `
             <td>${nombre}</td>
             <td>${dni}</td>
             <td>${fecha}</td>
-            <td>${num_asiento}</td>
+            <td>${silla}</td>
             <td>${clase}</td>
         `;
         tabla.appendChild(fila);
     }
-
-
-
-})
+});
